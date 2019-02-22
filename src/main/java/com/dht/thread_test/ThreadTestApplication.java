bac97a395c6573bfbf5f581b548103b7c205298f
dahengtong <dahengtong@beadwallet.com>
2019-02-22 12:43:27 +0800

    init project

---
+++ src/main/java/com/dht/thread_test/ThreadTestApplication.java
@@ -0,0 +1,13 @@
+package com.dht.thread_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThreadTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThreadTestApplication.class, args);
+    }
+
+}
